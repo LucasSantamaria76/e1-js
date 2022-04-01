a0d0e44a8bb18fbcf9e11a4721c126f26113b3fe
76luc <santamaria.luc76@gmail.com>
2022-04-01 09:55:35 -0300

    first commit

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const paresImpares = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     arr[i].length % 2 ? impares.push(arr[i]) : pares.push(arr[i]);
   }
-  console.log(`Los ingredientes pares son: ${pares}`);
   console.log(`Los ingredientes impares son: ${impares}`);
+  console.log(`Los ingredientes pares son: ${pares}`);
 };
 
 paresImpares(ingredientes);
