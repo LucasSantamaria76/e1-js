19dcbbc066a7d85f4ce8820fbb3a03d15402ebd9
LucasSantamaria76 <luc_1976@hotmail.com>
2022-04-01 08:32:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const ingredientes = [
+  "harina",
+  "levadura",
+  "aceite",
+  "sal",
+  "salsa",
+  "jamón",
+  "muzzarella",
+  "orégano",
+  "aceitunas",
+  "morrón",
+];
+
+const pares = [],
+  impares = [];
+
+const paresImpares = (arr) => {
+  if (!arr.length) {
+    console.log("Lista vacía");
+    return;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].length % 2 ? impares.push(arr[i]) : pares.push(arr[i]);
+  }
+  console.log(`Los ingredientes pares son: ${pares}`);
+  console.log(`Los ingredientes impares son: ${impares}`);
+};
+
+paresImpares(ingredientes);
